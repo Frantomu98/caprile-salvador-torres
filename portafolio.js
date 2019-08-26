6efb83889549f5ff9b554b16b5f6196c0dc61244
Frantomu98 <53983504+Frantomu98@users.noreply.github.com>
2019-08-25 22:08:59 -0400

    Update portafolio.js

--- portafolio.js
+++ portafolio.js
@@ -33,41 +33,41 @@ var trabajos = [{
     figure: "media/TANIN.png",
     alt: "Descripción de la imagen",
     title: "Tanin",
-    about: "Tanin es una pequeña barra 100% de mate deshidratado y luego prensado, por lo que posee fuertes cualidades energizantes",
+    about: "Producto alimenticio hecho 100% de mate. Creado para la Feria Gastronómica de Taller: Producto, 2018.",
     autor: "Francisca Torres y Luciano Caprile",
 }, {
     figure: "media/esplat.png",
     alt: "Descripción de la imagen",
     title: "Esplat",
-    about: "Esplat es un Bowl que muele alimentos tales como paltas, papas o zapallos separándolos de su cáscara en pocos pasos. ",
+    about: "Prensador de alimentos para Ilko, muele paltas, papas y otras verduras hervidas separándolas de su cáscara. TIDE, 2019. ",
     autor: "Francisca Torres y Luciano Caprile",
     url:"https://www.youtube.com/watch?v=MBFTqAlNHzg&feature=youtu.be"
 }, {
     figure: "media/wako.png",
     alt: "Descripción de la imagen",
     title: "Wako",
-    about: "Wako es un mudador para bebés portátil y plegable para situaciones de emergencias.",
+    about: "Wako es un lavatorio/mudador para bebés. Es portátil y plegable, pensado para un contexto de emergencia. Taller: Producto 2018.",
     autor: "Francisca Torres y Luciano Caprile",
     url:"https://www.youtube.com/watch?v=WoQnWIaZv_s&feature=youtu.be"
 }, {
     figure: "media/eclipse.png",
     alt: "Descripción de la imagen",
     title: "Eclipse",
-    about: "Luminaria experimental creada a partir de madera noble y clavos. Examen iluminación. ",
+    about: "Luminaria experimental creada a partir de madera noble y clavos. Examen Iluminación. ",
     autor: "Tomás Salvador",
     url: "https://www.instagram.com/avstrak.t/"
 }, {
     figure: "media/mantis.png",
     alt: "Descripción de la imagen",
     title: "Mantis",
-    about: "Mantis es un equipo desarrollado para jugar Coreball, un nuevo deporte el cual ayuda a fortalecer la zona de los estabilizadores del cuerpo.",
+    about: "Mantis es un equipo desarrollado para jugar Coreball, un nuevo deporte el cual ayuda a fortalecer la zona de los estabilizadores del cuerpo. Taller: Producto, 2018.",
     autor: "Francisca Torres",
     url:"https://youtu.be/wnWEpwL90YI"
 }, {
     figure: "media/moviles.png",
     alt: "Descripción de la imagen",
     title: "Móviles",
-    about: "Línea de juguetes semi habitables busca fomentar la creatividad en infanfantes además de fomentar vínculos con sus padres",
+    about: "Móviles, la nueva línea de de Magia y Cartón presenta Rola: el auto. Este juguete busca fomentar la creatividad y fortalecer el lazo con los padres. TIDE, 2019",
     autor: "Francisca Torres y Luciano Caprile",
     url:"https://www.youtube.com/watch?v=9gdR-9AKXEQ&feature=youtu.be"
 
